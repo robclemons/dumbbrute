0a190cf6d1d388b83771312be9885ed768f59bdd
Robbie Clemons <robquad@ZED-2.(none)>
2010-03-16 15:00:27 -0400

    multiprocessing now supported

--- bruteforcer.c
+++ bruteforcer.c
@@ -3,44 +3,50 @@
 #include <time.h>
 #include <math.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
 #include "sha512test.h"
 
 #define NUM_CHARS 7
-
-int brute(int count, char *salt, char *crypt, char *passchars, char *pass)
+#define NUM_PROCESSES 2
+int brute(int pNum, char *salt, char *crypt, char *passchars)
 {
-	int total = count;	
-	int passIndex = 0;
-	int i;
-	for(i = 1;pow(NUM_CHARS,i) < total;++i)
+	char pass[25];
+	int count;
+	for(count = pNum; count < 100842; count += NUM_PROCESSES)
 	{
-		total -= pow(NUM_CHARS,i);
-	}
-	--total;
+		int total = count;	
+		int passIndex = 0;
+		int i;
+		for(i = 1;pow(NUM_CHARS,i) < total;++i)
+		{
+			total -= pow(NUM_CHARS,i);
+		}
+		--total;
 
-	int j;
-	int tmp = total;
-	for(j = i; j > 1; --j)
-	{
-		tmp = total / pow(NUM_CHARS,j-1);
-		pass[passIndex] = passchars[tmp];
-		total -= tmp * pow(NUM_CHARS, j-1);
+		int j;
+		int tmp = total;
+		for(j = i; j > 1; --j)
+		{
+			tmp = total / pow(NUM_CHARS,j-1);
+			pass[passIndex] = passchars[tmp];
+			total -= tmp * pow(NUM_CHARS, j-1);
+			++passIndex;
+		}
+		int chrIndex = total % NUM_CHARS;
+		pass[passIndex] = passchars[chrIndex];
 		++passIndex;
-	}
-	int chrIndex = total % NUM_CHARS;
-	pass[passIndex] = passchars[chrIndex];
-	++passIndex;
-	pass[passIndex] ='\0';
-//	printf("trying: %s\n",pass);
-	char *result = __sha512_crypt(pass, salt);
-	if(strcmp(result, crypt) == 0)
-	{
+		pass[passIndex] ='\0';
+	//	printf("trying: %s\n",pass);
+		char *result = __sha512_crypt(pass, salt);
+		if(strcmp(result, crypt) == 0)
+		{
 		
-		printf("password: %s, it took %d hashes\n", pass, count);
-		return 1;
+			printf("password: %s, it took %d hashes\n", pass, count);
+			return count;
+		}
+	//	printf("%d\n", count);
 	}
-//	printf("%d\n", count);
-	return 0;
 
 }
 int main(int argc, const char** argv)
@@ -50,23 +56,26 @@ int main(int argc, const char** argv)
 	char *crypt = "$6$Y72m9KTGYlKZl6zA$S4.P9s8hkiUflpkZoGpjikGvvOLSV7iFf5DEuqMHZobaAB/lvz4cYl5JyEK2aB4k6Xu/5s0NNOaXa1ua6wqMc1";
 	char *salt = "$6$Y72m9KTGYlKZl6zA";
 	char passchars[NUM_CHARS] = "bcdo123";
-	char pass[25];
-	clock_t start, finish;
-	start = clock();
-	int i;
+	time_t start, finish;
+	start = time(NULL);
 	int found = 0;
-	for(i = 1; i < 100842; i++)
+	int i;
+	pid_t pid;
+	for(i = 1; i <= NUM_PROCESSES; ++i)
 	{
-		
-		found = brute(i, salt, crypt, passchars, &pass);
-		if(found)
-		{
-			finish = clock();
-			int time = (finish - start)/CLOCKS_PER_SEC;
-			printf("It took %d seconds\n",time);
-			printf("averaged %f c/s\n", (float)i / time);
-			exit(0);
-		}
+		pid = fork();
+		if(pid == 0)
+			found = brute(i, salt, crypt, passchars);
+	}
+	if(pid > 0)
+		wait();
+	if(found != 0)
+	{
+		finish = time(NULL);
+		int time = difftime(finish, start);
+		printf("It took %d seconds\n",time);
+		printf("averaged %f c/s\n", (float)found / time);
+		kill(0, SIGKILL);
 	}
         return 0;
 }
