8bf186d7ddd74eba07ad762bf2743548396c6dcd
Robbie Clemons <robclemons@gmail.com>
2010-04-23 19:41:51 -0400

    Heavy refactoring of brutus.py- now with real hashing action!
    Seems to work now. Minor other changes- removed copying.lesser
    as we no longer distribute crypt, and fixed test.py so it
    works... we hope.
    
    Renamed brutus.py to dumbbrute 'cause... its the main entry
    point into the program now, and the project is called dumbbrute...

--- dumbbrute.py
+++ dumbbrute.py
@@ -60,19 +60,19 @@ def num_passwords(charset, maximum_password_length):
 		possibleNum += (len(charset) ** each)
 	return possibleNum
 	
-def start_server():
+def start_server(log_requests, port):
 	host = ''
-	port = 8000
 	done = False
-	server = SimpleXMLRPCServer((host, port), SimpleXMLRPCRequestHandler, allow_none=True)
+	server = SimpleXMLRPCServer((host, port), SimpleXMLRPCRequestHandler,
+			allow_none=True, logRequests=log_requests)
 	bruteforcer = Bruteforcer()
 	server.register_instance(bruteforcer)
 	server.register_function(cpu_count)
 	server.serve_forever()
 
-def start_local(num_threads, max_pw_len, charset, hash_value):
-	start_new_thread(start_server, tuple())
-	proxy = ServerProxy("http://localhost:8000")
+def start_local(num_threads, max_pw_len, charset, hash_value, port, verbose):
+	start_new_thread(start_server, (verbose, port))
+	proxy = ServerProxy("http://localhost:" + str(port))
 	if not proxy.heartbeat():
 		raise Exception("Could not connect to local server")
 	start = 0
@@ -92,12 +92,13 @@ def start_local(num_threads, max_pw_len, charset, hash_value):
 			if not result[0]:
 				all_done = False
 			elif len(result[1]):
-				return result
+				return result[1]
 	return False
 	
-def start_master(charset, addr_list, hash_value):
+def start_master(charset, max_pw_len, nodefile, hash_value):
 	
 	# create the proxy list
+	addr_list = open(nodefile).readlines()
 	proxy_list = [ServerProxy(addr) for addr in addr_list]
 	# make sure all servers are alive
 	for proxy in proxy_list:
@@ -107,37 +108,73 @@ def start_master(charset, addr_list, hash_value):
 	# get the count of cpus in the cluster
 	cpus_per_machine = {proxy: proxy.cpu_count() for proxy in proxy_list}
 	total_cpus = sum(cpus_per_machine.values())
+	# get the number of passwords and passwords/thread
+	start = 0
+	end = num_passwords(charset, max_pw_len)
+	hashes_per_thread = ceil(end/total_cpus)
+	
 	# now start the bruteforcing
-	current_threads = 0
+	results = []
 	for proxy in proxy_list:
 		proxy_capacity = cpus_per_machine[proxy]
-		start_new_thread(bruteforce_wrapper, (proxy, current_threads, current_threads + proxy_capacity, total_cpus, charset, hash_value))
-		current_threads += proxy_capacity
+		for i in range(proxy_capacity):
+			tmp = start
+			start += hashes_per_thread
+			job_id = proxy.bruteforce(tmp, start, charset, hash_value)
+			results.append((proxy, job_id))
 		
 	# poll the machines on a 1 minute interval asking for results
-	while not done:
-		sleep(5)
+	password = ""
+	still_running = True
+	while still_running:
+		sleep(10)
+		still_running = False
+		for proxy, job_id in results:
+			status, password = proxy.done(job_id)
+			if not status: still_running = True
+			if password: break
 	
-	for proxy in proxy_list:
-		proxy.stop()
+	for proxy, job_id in results:
+		proxy.kill(job_id)
+		
+	return password
 
 	
 if __name__ == "__main__":
 	
-	if len(argv) == 1:
-		start_server()
-	elif len(argv) == 5:
-		num_threads = int(argv[1])
-		max_pass_len = int(argv[2])
-		charset = argv[3]
-		hash_value = argv[4]
-		result = start_local(num_threads, max_pass_len, charset, hash_value)
-		if result:
-			print(result[1])
-		else:
-			print("The password was not found")		
-	elif len(argv) == 6:
-		charset = argv[1]
-		ip_list = open(argv[2]).read().split()
-		hash_value = argv[3]
-		start_master(charset, ip_list, hash_value)
+	from optparse import OptionParser
+	from string import ascii_lowercase
+	
+	parser = OptionParser()
+	
+	parser.add_option("-t", "--threads", dest="threads", type="int",
+		help="The number of threads to run with", default=cpu_count())
+	
+	parser.add_option("-l", "--length", dest="length", type="int",
+		help="The maximum password length to look for", default=8)
+		
+	parser.add_option("-c", "--charset", dest="charset",
+		help="The set of possible characters in the password", default=ascii_lowercase)
+		
+	parser.add_option("-n", "--nodes", dest="nodes", default=False,
+		help="File location containing a newline delimited list of compute nodes")
+		
+	parser.add_option("-p", "--port", dest="port", type="int",
+		help="The port to contact the server on", default=8000)
+		
+	parser.add_option("-v", "--verbose", action="store_true", help="Emit server logs")
+
+	opts, args = parser.parse_args()
+	
+	if not len(args):
+		start_server(opts.verbose, opts.port)
+
+	elif opts.nodes:
+		retval = start_master(opts.charset, opts.length, opts.nodes, args[0])
+		if retval: print(retval)
+		else: print("The password was not found")
+		
+	else:
+		result = start_local(opts.threads, opts.length, opts.charset, args[0], opts.port, opts.verbose)
+		if result: print(result)
+		else: print("The password was not found")
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import brutus
 b1 = brutus.Brute(1, 5000, "bo123", "$6$Y72m9KTGYlKZl6zA$S4.P9s8hkiUflpkZoGpjikGvvOLSV7iFf5DEuqMHZobaAB/lvz4cYl5JyEK2aB4k6Xu/5s0NNOaXa1ua6wqMc1", "$6$Y72m9KTGYlKZl6zA$")
 b2 = brutus.Brute(5001, 10000, "bo123", "$6$Y72m9KTGYlKZl6zA$S4.P9s8hkiUflpkZoGpjikGvvOLSV7iFf5DEuqMHZobaAB/lvz4cYl5JyEK2aB4k6Xu/5s0NNOaXa1ua6wqMc1", "$6$Y72m9KTGYlKZl6zA$")
